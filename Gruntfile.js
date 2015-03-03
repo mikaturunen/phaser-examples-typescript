982d0abee0a9f006ca24a30c8bbf60a85f82d016
Mika Turunen <mika.turunen@ymail.com>
2015-03-03 17:14:08 +0200

    Minor silly tweaks on gruntfile formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,31 @@
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks("grunt-contrib-connect");
 
-    grunt.loadTasks('./tasks');
+    grunt.loadTasks("./tasks");
 
     grunt.initConfig({
-
         examples: {
             all: {
-            options: {
-                base: 'examples',
-                excludes: ['_site', 'assets', 'states', 'wip']
-            },
-            src: ['examples/**/*.js'],
-            dest: 'examples/_site/examples.json'
+                options: {
+                    base: "examples",
+                    excludes: ["_site", "assets", "states", "wip"]
+                },
+                src: [ "examples/**/*.js" ],
+                dest: "examples/_site/examples.json"
             }
         },
-
         connect: {
             root: {
                 options: {
                     keepalive: true,
-                    hostname: '*',
+                    hostname: "*",
                     port: 8001
                 }
             }
         }
-
     });
 
-    grunt.registerTask('default', ['examples']);
+    grunt.registerTask("default", [ "examples" ]);
 
 };

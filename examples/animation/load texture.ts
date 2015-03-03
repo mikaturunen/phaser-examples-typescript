3e04130d5e25fe78efea0fe77c249cccf77a4cfc
Mika Turunen <mika.turunen@ymail.com>
2015-03-03 17:54:10 +0200

    Example: load texture written in TypeScript

---
+++ examples/animation/load texture.ts
@@ -0,0 +1,29 @@
+
+
+var sprite: Phaser.Sprite;
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { 
+    preload: () => {
+         game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
+        game.load.spritesheet('monster', 'assets/sprites/metalslug_monster39x40.png', 39, 40);
+    },
+
+    create: () => {
+        sprite = game.add.sprite(300, 200, 'monster');
+
+        sprite.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        sprite.animations.play('walk', 20, true);
+        sprite.scale.set(4);
+        sprite.smoothed = false;
+
+        game.input.onDown.add(changeTexture, this);
+    }
+});
+
+function changeTexture() {
+    if (sprite.key === 'monster') {
+        sprite.loadTexture('mummy', 0, false);
+    } else {
+        sprite.loadTexture('monster', 0, false);
+    }
+}

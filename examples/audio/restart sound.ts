037f87e1bc2953bbfeb9243e30bb03ba8bbbbd5e
Mika Turunen <mika.turunen@ymail.com>
2015-03-03 18:32:30 +0200

    Example restart sound written in TypeScript

---
+++ examples/audio/restart sound.ts
@@ -0,0 +1,32 @@
+
+var s: Phaser.Sprite;
+var music: Phaser.Audio;
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+    preload: () => {
+        game.load.image("greenie", "assets/sprites/wizball.png");
+        game.load.audio("wizball", ["assets/audio/oedipus_wizball_highscore.mp3", "assets/audio/oedipus_wizball_highscore.ogg"]);
+    },
+
+    create: () => {
+        game.stage.backgroundColor = "#182d3b";
+        game.input.touch.preventDefault = false;
+
+        music = game.add.audio("wizball");
+
+        music.play();
+
+        s = game.add.sprite(game.world.centerX, game.world.centerY, "greenie");
+        s.anchor.set(0.5);
+
+        game.input.onDown.add(() => music.restart(), this);
+    }, 
+
+    update: () => {
+        s.rotation += 0.01;
+    }, 
+    
+    render: () => {
+        game.debug.soundInfo(music, 20, 32);
+    } 
+});

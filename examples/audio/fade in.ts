d2191c05d784bce93835cfef96817f4442c775b7
Mika Turunen <mika.turunen@ymail.com>
2015-03-03 18:14:58 +0200

    Example: fade in writtein in TypeScript

---
+++ examples/audio/fade in.ts
@@ -0,0 +1,24 @@
+
+var music: Phaser.Audio;
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+    preload: () => {
+        game.load.image("disk", "assets/sprites/ra_dont_crack_under_pressure.png");
+        game.load.audio("boden", "assets/audio/goaman_intro.mp3");
+    }, 
+
+    create: () => {
+        game.stage.backgroundColor = "#182d3b";
+        game.input.touch.preventDefault = false;
+
+        music = game.add.audio("boden");
+        music.onDecoded.add(() => music.fadeIn(4000), this);
+
+        s: Phaser.Sprite = game.add.sprite(game.world.centerX, game.world.centerY, "disk");
+        s.anchor.setTo(0.5, 0.5);
+    }, 
+
+    render: () => {
+        game.debug.soundInfo(music, 20, 32);
+    }
+});

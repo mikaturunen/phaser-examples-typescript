888682de288429b789c5b4931f088a2b268a6b11
Mika Turunen <mika.turunen@ymail.com>
2015-03-03 17:49:00 +0200

    Example: destroy animation written in typescript

---
+++ examples/animation/destroy animation.ts
@@ -0,0 +1,41 @@
+
+var sprites: Phaser.Group;
+var rip: number = 0;
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+    preload: () => {
+        game.load.spritesheet("mummy", "assets/sprites/metalslug_mummy37x45.png", 37, 45, 18);
+    }, 
+
+    create: () => {
+        sprites = game.add.group();
+        game.time.events.loop(50, createSprite, this);
+    }, 
+
+    update: () => {
+        sprites.setAll("x", 10, true, true, 1);
+        sprites.forEach(checkSprite, this, true);
+    },
+
+    render: () => {
+        game.debug.text("Group size: " + sprites.total, 32, 32);
+        game.debug.text("Destroyed: " + rip, 32, 64);
+    } 
+});
+
+function createSprite() {
+    var mummy: Phaser.Sprite = sprites.create(0, game.world.randomY, "mummy");
+    mummy.animations.add("walk");
+    mummy.play("walk", 10, true);
+}
+
+function checkSprite(sprite) {
+    try {
+        if (sprite.x > game.width) {
+            rip++;
+            sprites.remove(sprite, true);
+        }
+    } catch (e) {
+        console.log(sprite);
+    }
+}

538c99fbb103b21d6edc01e83d3ee74505617643
Mika Turunen <mika.turunen@ymail.com>
2015-03-09 20:09:34 +0200

    #2 Example draw group written in TypeScript

---
+++ examples/bitmapdata/draw group.ts
@@ -0,0 +1,26 @@
+
+var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+    preload: () => {
+        game.load.image("bg", "assets/pics/undersea.jpg");
+        game.load.image("loop", "assets/sprites/beball1.png");
+    }, 
+
+    create: () => {
+        game.add.sprite(0, 0, "bg");
+
+        var group: Phaser.Group = game.make.group();
+
+        //  Add a bunch of sprites in random positions to the group
+        for (var i = 0; i < 40; i++) {
+            group.create(game.world.randomX, game.world.randomY, "loop");
+        }
+
+        //  This is the BitmapData we"re going to be drawing to
+        var bmd = game.add.bitmapData(game.width, game.height);
+
+        bmd.addToWorld();
+
+        //  Draw the group
+        bmd.drawGroup(group);
+    } 
+});

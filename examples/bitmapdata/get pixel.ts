ec3a57a832fcd31dfacb31fe9acade0717d4e654
Mika Turunen <mika.turunen@ymail.com>
2015-03-09 20:17:44 +0200

    #2 Example get pixel written in TypeScript

---
+++ examples/bitmapdata/get pixel.ts
@@ -0,0 +1,33 @@
+
+var bmd: Phaser.BitmapData;
+var tooltip: Phaser.BitmapData;
+var sprite: Phaser.Sprite;
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example", { 
+	preload: () => game.load.image("wheel", "assets/pics/color_wheel_swirl.png"), 
+
+	create: () => {
+		bmd = game.make.bitmapData(800, 600);
+		bmd.draw("wheel", -200, -100);
+		bmd.update();
+		bmd.addToWorld();
+
+		tooltip = game.make.bitmapData(64, 64);
+		sprite = game.add.sprite(0, 0, tooltip);
+
+		game.input.addMoveCallback(getPixelUpdateTooltip, this);
+	}
+});
+
+function getPixelUpdateTooltip (pointer: Phaser.Pointer, x: number, y: number) {
+
+	if (x >= 0 && x <= bmd.width && y >= 0 && y <= bmd.height) {
+		var color: any = bmd.getPixelRGB(x, y);
+
+		tooltip.fill(0, 0, 0);
+		tooltip.rect(1, 1, 62, 62, color.rgba);
+	
+		sprite.x = x;
+		sprite.y = y;
+	}
+}

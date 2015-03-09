6c4944c8b685c2b97be564f36535395e1708d21d
Mika Turunen <mika.turunen@ymail.com>
2015-03-09 20:32:58 +0200

    #2 example wobble written in TypeScript

--- examples/bitmapdata/wobble.js
+++ examples/bitmapdata/wobble.js
@@ -1,59 +1,47 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
-
-function preload() {
-
-    game.load.image('ball', 'assets/sprites/shinyball.png');
-
-}
-
-var bmd;
+var bmd: Phaser.BitmapData;
 
 var waveSize = 8;
 var wavePixelChunk = 2;
-var waveData;
-var waveDataCounter;
+var waveData: any;
+var waveDataCounter: any;
 
-function create() {
+var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+	preload: () => game.load.image("ball", "assets/sprites/shinyball.png"), 
 
-	//	Create our BitmapData object at a size of 32x64
-	bmd = game.add.bitmapData(32, 64);
+	create: () => {
+		//	Create our BitmapData object at a size of 32x64
+		bmd = game.add.bitmapData(32, 64);
 
-    //  And apply it to 100 randomly positioned sprites
-    for (var i = 0; i < 100; i++)
-    {
-        game.add.sprite(game.world.randomX, game.world.randomY, bmd);
-    }
+	    //  And apply it to 100 randomly positioned sprites
+	    for (var i = 0; i < 100; i++) {
+	        game.add.sprite(game.world.randomX, game.world.randomY, bmd);
+	    }
 
-    //  Populate the wave with some data
-	waveData = game.math.sinCosGenerator(32, 8, 8, 2);
+	    //  Populate the wave with some data
+		waveData = game.math.sinCosGenerator(32, 8, 8, 2);
+	}, 
 
-}
+	update: () => {
+		//	Clear the BitmapData
+		bmd.cls();
 
-function update() {
-
-	//	Clear the BitmapData
-	bmd.cls();
-
-	updateWobblyBall();
-
-}
+		updateWobblyBall();
+	} 
+});
 
 //  This creates a simple sine-wave effect running through our BitmapData.
 //  This is then duplicated across all 100 sprites using it, meaning we only have to calculate it and upload it to the GPU once.
-
 function updateWobblyBall() {
-
 	var s = 0;
 	var copyRect = { x: 0, y: 0, w: wavePixelChunk, h: 32 };
 	var copyPoint = { x: 0, y: 0 };
 
-	for (var x = 0; x < 32; x += wavePixelChunk)
-	{
+	for (var x = 0; x < 32; x += wavePixelChunk) {
 		copyPoint.x = x;
 		copyPoint.y = waveSize + (waveSize / 2) + waveData.sin[s];
 
-		bmd.context.drawImage(game.cache.getImage('ball'), copyRect.x, copyRect.y, copyRect.w, copyRect.h, copyPoint.x, copyPoint.y, copyRect.w, copyRect.h);
+		bmd.context.drawImage(game.cache.getImage("ball"), copyRect.x, copyRect.y, copyRect.w, copyRect.h, copyPoint.x, copyPoint.y, copyRect.w, copyRect.h);
 			
 		copyRect.x += wavePixelChunk;
 			
@@ -61,7 +49,7 @@ function updateWobblyBall() {
 	}
 
 	//	Now all the pixel data has been redrawn we render it to the BitmapData object.
-	//	In CANVAS mode this doesn't do anything, but on WebGL it pushes the new texture to the GPU.
+	//	In CANVAS mode this doesn"t do anything, but on WebGL it pushes the new texture to the GPU.
 	//	If your game is exclusively running under Canvas you can safely ignore this step.
 	bmd.render();
 
@@ -70,8 +58,7 @@ function updateWobblyBall() {
 	
 	waveDataCounter++;
 	
-	if (waveDataCounter == waveData.length)
-	{
+	if (waveDataCounter == waveData.length) {
 		waveDataCounter = 0;
 	}
 

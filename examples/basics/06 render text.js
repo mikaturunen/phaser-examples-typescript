22c88e8ab14b5212358ff9ba1a8a3fac87197815
Mika Turunen <mika.turunen@ymail.com>
2015-03-09 19:58:55 +0200

    #2 Example render text written in TypeScript

--- examples/basics/06 render text.js
+++ examples/basics/06 render text.js
@@ -1,11 +1,14 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create });
-
-function create() {
-
-    var text = "- phaser -\n with a sprinkle of \n pixi dust.";
-    var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
-
-    var t = game.add.text(game.world.centerX-300, 0, text, style);
-
-}
+var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+    create: () => {
+        var text: string = "- phaser -\n with a sprinkle of \n pixi dust.";
+        
+        var style: any = { 
+            font: "65px Arial", 
+            fill: "#ff0044", 
+            align: "center" 
+        };
+        
+        var t: Phaser.Text = game.add.text(game.world.centerX-300, 0, text, style);
+    } 
+});

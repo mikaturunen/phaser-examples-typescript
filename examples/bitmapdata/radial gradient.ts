03b91cf1284a6d5b325c93e4538cc51e8baa1ba6
Mika Turunen <mika.turunen@ymail.com>
2015-03-09 20:24:51 +0200

    #2 Example radial gradient written in TypeScript

---
+++ examples/bitmapdata/radial gradient.ts
@@ -0,0 +1,29 @@
+
+var bmd: Phaser.BitmapData;
+var innerCircle: Phaser.Circle;
+var outerCircle: Phaser.Circle;
+
+var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example", { 
+    create: () => {
+        //  Our BitmapData (same size as our canvas)
+        bmd = game.make.bitmapData(800, 600);
+
+        //  Add it to the world or we can"t see it
+        bmd.addToWorld();
+
+        //  Create the Circles
+        innerCircle = new Phaser.Circle(200, 200, 100);
+        outerCircle = new Phaser.Circle(200, 200, 300);
+
+        game.add.tween(innerCircle).to( { x: 100, y: 100, radius: 1 }, 3000, "Sine.easeInOut", true, 0, -1, true);
+    }, 
+
+    update: () => {
+        var grd: any = bmd.context.createRadialGradient(innerCircle.x, innerCircle.y, innerCircle.radius, outerCircle.x, outerCircle.y, outerCircle.radius);
+        grd.addColorStop(0, "#8ED6FF");
+        grd.addColorStop(1, "#003BA2");
+
+        bmd.cls();
+        bmd.circle(outerCircle.x, outerCircle.y, outerCircle.radius, grd);
+    } 
+});

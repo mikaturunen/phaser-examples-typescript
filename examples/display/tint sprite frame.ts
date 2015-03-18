c9ca3d2b9cefa8a8d53bcb7d603e4c1584082afc
Mika Turunen <mika.turunen@ymail.com>
2015-03-18 19:53:55 +0200

    #6 example tint sprite frame written in typescript

--- examples/display/tint sprite frame.ts
+++ examples/display/tint sprite frame.ts
@@ -1,18 +1,25 @@
-var _this = this;
-var jellyfish;
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", {
-    preload: function () {
+
+var jellyfish: Phaser.Sprite;
+
+var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser-example", { 
+    preload: () => {
         game.load.atlas("seacreatures", "assets/sprites/seacreatures_json.png", "assets/sprites/seacreatures_json.json");
         game.load.image("undersea", "assets/pics/undersea.jpg");
         game.load.image("coral", "assets/pics/seabed.png");
-    },
-    create: function () {
+    }, 
+
+    create: () => {
         game.add.image(0, 0, "undersea");
+
         jellyfish = game.add.sprite(370, 200, "seacreatures");
+
         jellyfish.animations.add("swim", Phaser.Animation.generateFrameNames("blueJellyfish", 0, 32, "", 4), 30, true);
         jellyfish.animations.play("swim");
+
         game.add.image(0, 466, "coral");
+
         game.add.tween(jellyfish).to({ y: "100" }, 8000, "Quad.easeInOut", true, 0, 1000, true);
-        game.time.events.loop(Phaser.Timer.SECOND * 2, function () { return jellyfish.tint = Math.random() * 0xffffff; }, _this);
-    }
+
+        game.time.events.loop(Phaser.Timer.SECOND * 2, () => jellyfish.tint = Math.random() * 0xffffff, this);
+    } 
 });

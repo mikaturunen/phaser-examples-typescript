032ee12213f1eb3094f1c934cae59137d0183958
Mika Turunen <mika.turunen@ymail.com>
2015-03-18 19:55:11 +0200

    #6 example tint sprite written in typescript

--- examples/display/tint sprite.js
+++ examples/display/tint sprite.js
@@ -1,25 +1,15 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var tintPic: Phaser.Sprite;
 
-function preload() {
+var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, "phaser-example", { 
+    preload: () => game.load.image("pic", "assets/pics/barbarian_loading.png"), 
 
-	game.load.image('pic', 'assets/pics/barbarian_loading.png');
+    create: () => {
+        tintPic = game.add.sprite(game.world.centerX, game.world.centerY, "pic");
+        tintPic.anchor.set(0.5);
 
-}
-
-var pic;
-
-function create() {
-
-	pic = game.add.sprite(game.world.centerX, game.world.centerY, 'pic');
-	pic.anchor.set(0.5);
-
-    game.time.events.loop(Phaser.Timer.SECOND * 2, changeTint, this);
-
-}
-
-function changeTint() {
-
-	pic.tint = Math.random() * 0xffffff;
-
-}
+        game.time.events.loop(Phaser.Timer.SECOND * 2, () => {
+            tintPic.tint = Math.random() * 0xffffff;
+        }, this);
+    } 
+});
